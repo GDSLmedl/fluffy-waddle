9e3ea9c32a77b6c76b9058bce9706f9443933a6a
Galynea <dauver_a@epita.fr>
2018-12-14 18:19:23 +0100

    First IA

---
+++ src/Game/PlayerIA/GdslmedlPlayer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Hackathon\PlayerIA;
+
+use Hackathon\Game\Result;
+
+/**
+ * Class GdslmedlPlayer
+ * @package Hackathon\PlayerIA
+ * @author Max-Emilien Dauvert-Latre
+ */
+class GdslmedlPlayer extends Player
+{
+    protected $mySide;
+    protected $opponentSide;
+    protected $result;
+
+    public function getChoice()
+    {
+        // -------------------------------------    -----------------------------------------------------
+        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
+        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
+        // -------------------------------------    -----------------------------------------------------
+        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
+        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
+        // -------------------------------------    -----------------------------------------------------
+        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
+        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
+        // -------------------------------------    -----------------------------------------------------
+        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
+        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
+        // -------------------------------------    -----------------------------------------------------
+        // How to get the stats                ?    $this->result->getStats()
+        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
+        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
+        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
+        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
+        // -------------------------------------    -----------------------------------------------------
+        // How to get the number of round      ?    $this->result->getNbRound()
+        // -------------------------------------    -----------------------------------------------------
+        // How can i display the result of each round ? $this->prettyDisplay()
+        // -------------------------------------    -----------------------------------------------------
+
+        $this->prettyDisplay();
+
+        $oppChoice = $this->result->getLastChoiceFor($this->opponentSide);
+        
+        if ($oppChoice === 0)
+            return parent::friendChoice();
+        return $oppChoice === parent::foeChoice() ? parent::foeChoice() : parent::friendChoice();
+    }
+ 
+};
